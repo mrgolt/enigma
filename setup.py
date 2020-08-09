d3b182da8fa809a58550a368d5165510e1a721f3
V <mrgolt>
2020-08-09 22:40:49 +0500

    add reqire-s and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='enigma',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='J Golt',
+    author_email='mrgolt404@gmail.com',
+    description=''
+)
