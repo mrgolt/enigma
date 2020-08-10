ebc170daf1b7840fea098bf7f5c3d66ac53f88f9
V <mrgolt>
2020-08-10 22:27:56 +0500

    add ea

--- src/index.php
+++ src/index.php
@@ -1,13 +1,11 @@
 <?php
+    print ($_REQUEST);
+
     $response = 'UPD_LATEST_VERSION';
     $cur_ver = "2.0.0";
 
     if ($_REQUEST['ea_name']=="Enigma") {
         if ($_REQUEST['ea_version']!="$cur_ver") $response = "UPD_OUTDATED_VERSION|$cur_ver|http://trendchaser.ru/enigma.ex4";
-    }
-    else
-    {
-    $response = "";
     }
 
     $response = iconv("utf-8","windows-1251",$response);
