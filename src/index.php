46898b38a0c2d749c54d26b33dbc3ef6918be32c
V <mrgolt>
2020-08-10 15:29:11 +0500

    fix index.php

--- src/index.php
+++ src/index.php
@@ -5,6 +5,10 @@
     if ($_REQUEST['ea_name']=="Expert") {
         if ($_REQUEST['ea_version']!="$cur_ver") $response = "UPD_OUTDATED_VERSION|$cur_ver|https://trendchaser.ru/checkupdates/Expert.ex4";
     }
+    else
+    {
+    $response = "";
+    }
 
     $response = iconv("utf-8","windows-1251",$response);
     echo $response;
