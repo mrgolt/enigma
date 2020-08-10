815d53b21512a4ede99a416844c81cf8231b2b10
V <mrgolt>
2020-08-10 22:12:30 +0500

    add ea

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
     $response = 'UPD_LATEST_VERSION';
-    $cur_ver = "2.00";
+    $cur_ver = "2.0.0";
 
-    if ($_REQUEST['ea_name']=="Expert") {
-        if ($_REQUEST['ea_version']!="$cur_ver") $response = "UPD_OUTDATED_VERSION|$cur_ver|https://trendchaser.ru/checkupdates/Expert.ex4";
+    if ($_REQUEST['ea_name']=="Enigma") {
+        if ($_REQUEST['ea_version']!="$cur_ver") $response = "UPD_OUTDATED_VERSION|$cur_ver|http://trendchaser.ru/enigma.ex4";
     }
     else
     {
