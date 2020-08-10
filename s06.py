a44387f6ca111141e2f28ccd8b5530f4f18954f9
V <mrgolt>
2020-08-10 12:43:21 +0500

    update s06 ip

--- s06.py
+++ s06.py
@@ -276,7 +276,7 @@ async def commands():
 
 async def start():
     server = await asyncio.start_server(
-        handle_echo, 'localhost', 9999)
+        handle_echo, '0.0.0.0', 9999)
 
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
